c67b07e3ffb287b61bc78159b3a482b3109fa696
AkhmedovaShakhnaz <107623498+AkhmedovaShakhnaz@users.noreply.github.com>
2024-06-04 14:46:05 +0200

    Update GANLoss_tensorflow.py

--- GANLoss_tensorflow.py
+++ GANLoss_tensorflow.py
@@ -4,13 +4,14 @@ EPSILON = 1e-8
 
 class GANetic(tensorflow.keras.losses.Loss):
     def __init__(
-    	self, 
-    	eps=1e-8,
-        reduction=None,
-    	name="ganetic_loss"):
+            	self, 
+            	eps=1e-8,
+                reduction=None,
+            	name="ganetic_loss"):
         super().__init__(name=name)
         self.name = name
         self.eps = eps
+        self.reduction = reduction
 
     def call(self, y_true, y_pred):
         y_pred = tensorflow.keras.backend.clip(y_pred, tensorflow.keras.backend.epsilon(), 1 - tensorflow.keras.backend.epsilon())
@@ -22,7 +23,7 @@ class GANetic(tensorflow.keras.losses.Loss):
         term_1_1 = tensorflow.math.multiply(3.985, term_1_0)
         term_1 = tensorflow.math.sqrt(tensorflow.math.add(tensorflow.math.abs(term_1_1), self.eps))
 
-        if reduction == "mean":
+        if self.reduction == "mean":
             loss = tensorflow.reduce_mean(term_0 + term_1)
         else:
             loss = term_0 + term_1
